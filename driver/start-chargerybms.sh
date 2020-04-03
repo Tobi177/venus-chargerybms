6bd72fc45d55e9c3e1f166ba5b990d5f72ae9454
Tobi177 <tobo@comodoweb.com>
2020-04-03 23:40:07 +0200

    fixed driver call

--- driver/start-chargerybms.sh
+++ driver/start-chargerybms.sh
@@ -9,7 +9,7 @@
 
 . /opt/victronenergy/serial-starter/run-service.sh
 
-app="/usr/bin/python /opt/victronenergy/chargerybms/readbms.py"
+app="/usr/bin/python /opt/victronenergy/chargerybms/chargerybms.py"
 verbose="-v --log-before 25 --log-after 25"
 timeout=3
 
